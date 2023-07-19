cf16e97e0777f77fb8f4f313d8f670e5f98bb84f
dung9788xx <42429112+dung9788xx@users.noreply.github.com>
2023-07-19 16:58:11 +0700

    Update SharingblogApplicationTests.java

--- src/test/java/com/nhom2/sharingblog/SharingblogApplicationTests.java
+++ src/test/java/com/nhom2/sharingblog/SharingblogApplicationTests.java
@@ -2,6 +2,7 @@ package com.nhom2.sharingblog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.jdbc.*;
 
 @SpringBootTest(exclude = {DataSourceAutoConfiguration.class })
 class SharingblogApplicationTests {
